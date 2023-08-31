bb2d94ce527b64f5db25da483654e6d99236ff91
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-08-31 11:24:11 +0900

    pick up time changes
    5 -- Converts the pick up time if a child is in 課外授業 and is 一号.  Children that are 一号 taking the after school class
         are exempt for charges resulting for being picked up late, up to a certain point.

--- transfer_time.py
+++ transfer_time.py
@@ -1,5 +1,5 @@
 """
-version 1.1
+Version 1.2
 
 First stable version that transfers drop off and pick up times from one excel file to another that then calculates
 the appropriate amount of money to charge.
@@ -11,7 +11,11 @@ General features
      files have been downloaded from hugnote, but will transfer data with what ever files are available.
 4 -- If children on the hugnote files(reference files) cannot be found in the 預かり料金表 then it will notify you which
      children cannot be found along with the class name.
+(new)
+5 -- Converts the pick up time if a child is in 課外授業 and is 一号.  Children that are 一号 taking the after school class
+     are exempt for charges resulting for being picked up late, up to a certain point.
 """
+
 
 import pandas as pd
 import tkinter as tk
@@ -19,11 +23,13 @@ from tkinter import filedialog
 from tkinter import messagebox
 from datetime import datetime
 import openpyxl
+from openpyxl import Workbook
 import os
 
 
 
-def find_date(tab, date):
+def find_date(tab: Workbook, date: datetime):
+    '''find the row and column (essentially the corordinates) of the matching date.'''
     for i, row in enumerate(tab.iter_rows()):
         for idx, cell in enumerate(row):
             if cell.value == date:
@@ -32,7 +38,8 @@ def find_date(tab, date):
 
 
 
-def find_name(tab1, name, date_row):
+def find_name(tab1: Workbook, name: str, date_row: int) -> list[int]: # speficically returning one int in the format of a list to avoid out of index errors.
+    '''find the row number of where the childs name is located in thr workbook'''
     ans = []
     for i, row in enumerate(tab1.iter_rows()):
         if type(row[2].value) == str:
@@ -48,13 +55,16 @@ def find_name(tab1, name, date_row):
 
 
 
-def arr_check_time(time:str) -> int:
+def arr_check_time(time: str) -> int:
+    '''Convert the arival time so that if anyone arrives before 7:30 it is set to 7:30'''
     time = int(time)
     if  time < 730:
         time = 730
     return time
 
-def dep_check_time(time:str) -> int:
+def dep_check_time(time: str) -> int:
+    '''Convert the departure time so that if they are 9 minuets over the time limit it reverts to the time limit
+        So that they are not charged'''
     time = int(time)
     if 1131 <= time <= 1139:
         time = 1130
@@ -63,9 +73,35 @@ def dep_check_time(time:str) -> int:
     return time
 
 
+def kagai_ichigo_check_time(name: str, time: int, day_of_week: int, sheet: Workbook) -> int:
+    '''Convert departure time for children that are in 課外授業 and are 一号 to 1430 if they have class that day and
+       they leave before the pick up time limit.'''
+    days_of_week = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
+    for i, row in enumerate(sheet):
+        #print(name, row[1].value)
+        name_val = row[1].value
+        if name_val != None:
+            name_val = name_val.replace('　','')  # replaceing the japanese space known as the IDEOGRAPHIC SPACE character or \u3000 in unicode escape character.
+            name_val = name_val.replace(' ', '')
+        if name == name_val:
+            time_limit = sheet[i+1][2 + day_of_week].value
+            if time_limit == None:
+                #print('修正なし: {}は{}に課外がありません。'.format(name, days_of_week[day_of_week]))
+                return time
+            elif 1430 < time <= time_limit:
+                print('修正あり:　{} {} --> 1430。'.format(name, time))
+                time = 1430
+                return time
+            elif time > time_limit:
+                print('修正なし: {}{}が限度の{}を超えてるため。'.format(name, time, time_limit))
+                return time
+    print('課外授業を休んでる？: ', name, time, days_of_week[day_of_week], date)
+    return time
+
+
+
 
 def update_excel_data(input_file, reference_files, output_file):
-
 
     # Read the input Excel file with openpyxl
     output_data = openpyxl.load_workbook(input_file, data_only=False, keep_vba=True)
@@ -77,9 +113,21 @@ def update_excel_data(input_file, reference_files, output_file):
         #print(key)
         reference_data[key] = pd.read_csv(val, parse_dates=['日付'])
 
+    # create a list of children that are 一号 and are in the 課外授業.
+    ichigo_kagai_sheet = input_data['1号課外']
+    ichigo_kagai = []
+    for row in ichigo_kagai_sheet:
+        name_val = row[1].value
+        if name_val != None:
+            name_val = name_val.replace('　', '') # replaceing the japanese space known as the IDEOGRAPHIC SPACE character or \u3000 in unicode escape character.
+            name_val = name_val.replace(' ', '')
+            ichigo_kagai.append(name_val)
+
+    print(ichigo_kagai)
+
     missing_children = set()
     # Iterate over the input data tabs
-    for sheet_name in input_data.sheetnames[2:]:
+    for sheet_name in input_data.sheetnames[2:11]:
 
         # access the sheet we are currently working on
         cur_sheet = input_data[sheet_name]
@@ -101,11 +149,18 @@ def update_excel_data(input_file, reference_files, output_file):
         #print(cur_sheet.max_row)
         # Iterate through the refference data
         for i, row in ref_data.iterrows():
+            global date
             date = row['日付']
             child_name = row['こども氏名'].replace(' ', '')
             child_name = child_name.replace('　', '') # also replace the Japanese space.
             arrive_time = row['出席時刻']
             departure_time = row['帰宅時刻']
+
+            # create day of week num to plug into function to check if the kids are in ichigo_kagai
+            clean_date = date.to_pydatetime()
+            day_of_week_num = clean_date.weekday()
+            day_of_week_str = clean_date.strftime('%A')
+            # print(day_of_week_num, day_of_week_str)
 
             # remove : from time stamp and skip procedure if it is a nan value.
             if isinstance(arrive_time, str):
@@ -116,6 +171,7 @@ def update_excel_data(input_file, reference_files, output_file):
 
             #find the corresponding date(cell row and col) date_coor[0] is the row and date_coor[1] is the column
             date_coor = find_date(cur_sheet, date)
+
             #print(i, date_coor, date)
 
             # check to see if date_coor is empty or is None, if so skip the date. because it can cause errors in the fillowing operations.
@@ -139,8 +195,15 @@ def update_excel_data(input_file, reference_files, output_file):
                 out_sheet.cell(name_coor[0] + 1, date_coor[1] + 1).value = adj_arrive_time # Add one to adjust for the 0 index created with the enumreate() function
             if isinstance(departure_time, str):
                 adj_departure_time = dep_check_time(departure_time)
+                if child_name in ichigo_kagai: # adjust time if the kids are in 課外授業　and are 一号.
+                    adj_departure_time = kagai_ichigo_check_time(child_name, adj_departure_time, day_of_week_num, ichigo_kagai_sheet)
+                #print(adj_arrive_time, adj_departure_time)
                 out_sheet.cell(name_coor[0] + 1, date_coor[1] + 2).value = adj_departure_time # Add one to adjust for the 0 index created with the enumreate() function
-    messagebox.showinfo('以下の子供が見つかりませんでした。', "ハグノートと預かり料金ファイルの子供の漢字が異なってる可能性があります。\n預かり料金ファイルの子供の名前を修正してください。:\n\n" + "\n".join(missing_children))
+
+    if missing_children:
+        messagebox.showinfo('以下の子供が見つかりませんでした。', "ハグノートと預かり料金ファイルの子供の漢字が異なってる可能性があります。\n預かり料金ファイルの子供の名前を修正してください。:\n\n" + "\n".join(missing_children))
+    else:
+        messagebox.showinfo('完了', 'データ転送が完了しました。')
 
 
 
