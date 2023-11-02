10374ddc059d97df3fea6d32c3162c44a1313a01
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-11-02 10:38:44 +0900

    add docs

---
+++ create_billing.py
@@ -0,0 +1,4 @@
+"""
+A script that will take the results from the transfer_time script and insert key data into a billing
+form that will be delivered to the parents for extra charges incurred during that month.
+"""
