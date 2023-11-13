7800a390734af35d0aabdc04eceeeb7584a8a665
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-11-13 13:23:41 +0900

    update

--- create_billing.py
+++ create_billing.py
@@ -177,7 +177,7 @@ def testtest(dic):
 
 if __name__ == '__main__':
     #count_charges()
-    #create_billing()
+    create_billing()
     #testtest(count_charges())
-    find_year(count_charges())
-    convert_reiwa(2024, 4)
+    #find_year(count_charges())
+    #convert_reiwa(2024, 4)
