f21c50fd9db3f869c03d32a8ff4f7a6cab1f1ae7
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-12-04 11:52:53 +0900

    add space

--- create_billing.py
+++ create_billing.py
@@ -222,6 +222,7 @@ def convert_date(date: str) -> str:
     ye_mo_da = date.split('-')
     month = ye_mo_da[1]
     date = ye_mo_da[2]
+    
     # I'm converting to int and then inserting into f string because the leading 0 in something like
     # '10月07日' was not desirable. instead we want something like '10月7日'. So converting to int gets
     # rid of the leading 0.
