7ca378eecf4d465f4e91ec487fa67a38e073d212
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2025-06-26 11:28:53 +0900

    update

--- transfer_time.py
+++ transfer_time.py
@@ -179,7 +179,7 @@ def kagai_ichigo_check_time(name: str, time: int, day_of_week: int, sheet: Works
         if name_val is not None:
             name_val = replace_all_spaces(name_val)
         if name == name_val:
-            time_limit = sheet[i + 1][2 + day_of_week].value
+            time_limit = sheet[i+1][2 + day_of_week].value
             if time_limit is None:
                 # print('修正なし: {}は{}に課外がありません。'.format(name, days_of_week[day_of_week]))
                 return time
